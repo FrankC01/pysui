048d3714d7f7d499b9775505c24cbe6626327f46
Frank V. Castellucci <frankiecast@gmail.com>
2023-07-14 04:59:46 -0400

    Release 0.30.0

--- pysui/sui/sui_clients/sync_client.py
+++ pysui/sui/sui_clients/sync_client.py
@@ -552,4 +552,14 @@ class SuiClient(_ClientMixin):
         """
         inargs: dict = locals().copy()
         inargs.pop("self")
+<<<<<<< HEAD
         return self.execute(QueryEvents(**inargs))
+=======
+        return QueryEvents(
+            query=query,
+            cursor=cursor,
+            limit=limit,
+            descending_order=descending_order,
+            # **inargs,
+        )
+>>>>>>> d961976 (Release 0.30.0)
